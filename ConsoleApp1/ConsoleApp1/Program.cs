54018922395d7ff5db4abce4c609f66d6c6dfd44
Igor Baranowski <ipadowski123@gmail.com>
2019-12-23 00:26:58 +0100

    czesc maciej

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Czesc Igor");
+            Console.WriteLine("Czesc Maciek");
         }
     }
 }
